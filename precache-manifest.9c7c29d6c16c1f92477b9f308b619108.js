ae7e71509d129e0b247d73da4e5d0acf551313f0
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2020-09-30 23:37:58 +0530

    Updates

---
+++ precache-manifest.9c7c29d6c16c1f92477b9f308b619108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f075d139a81d0253d8e6251604bcb5ae",
+    "url": "/cgcoronatracker/index.html"
+  },
+  {
+    "revision": "0f1b7434c43afadca5f3",
+    "url": "/cgcoronatracker/static/css/main.786ebc92.chunk.css"
+  },
+  {
+    "revision": "22af5ac4892754c6ec62",
+    "url": "/cgcoronatracker/static/js/2.782822d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cgcoronatracker/static/js/2.782822d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1b7434c43afadca5f3",
+    "url": "/cgcoronatracker/static/js/main.8c4112d2.chunk.js"
+  },
+  {
+    "revision": "f119c8173cdd24dc0883",
+    "url": "/cgcoronatracker/static/js/runtime-main.045a0c97.js"
+  }
+]);
